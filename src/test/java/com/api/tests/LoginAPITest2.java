717b1cb2878b4c16d55b055290c9ef2697c77d25
Raubin Kumar <raubinkumar4321@gmail.com>
2026-01-10 12:08:38 +0530

    Base code

---
+++ src/test/java/com/api/tests/LoginAPITest2.java
@@ -0,0 +1,27 @@
+package com.api.tests;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class LoginAPITest2 {
+	
+	@Test(description = "Verify if Login API is working...")
+	public void loginTest() {
+		
+		Response response = given().baseUri("http://64.227.160.186:8080")
+				.header("Content-Type","application/json")
+				.body("{\r\n"
+				+ "  \"username\": \"Raubin\",\r\n"
+				+ "  \"password\": \"Munna@4457\"\r\n"
+				+ "}").post("/api/auth/login");
+		
+		System.out.println(response.asPrettyString());
+		Assert.assertEquals(response.getStatusCode(), 200);
+		
+	}
+
+}
