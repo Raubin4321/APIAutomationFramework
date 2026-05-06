09ee37c7928643782580d558bb3656a2ab5e3726
Raubin Kumar <raubinkumar4321@gmail.com>
2026-05-06 19:44:31 +0530

    Upadated LoginAPITest and .gitignore

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -1,25 +1,36 @@
 package com.api.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+import com.api.base.AuthService;
+import com.api.models.request.LoginRequest;
+import com.api.models.response.LoginResponse;
 
+import io.restassured.response.Response;
+
+@Listeners(com.api.listeners.TestListener.class)
 public class LoginAPITest {
 	
 	@Test(description = "Verify if Login API is working...")
 	public void loginTest() {
-		RestAssured.baseURI = "http://64.227.160.186:8080";
-		RequestSpecification x = RestAssured.given().header("Content-Type","application/json");
-		RequestSpecification y = x.body("{\r\n"
-				+ "  \"username\": \"Raubin\",\r\n"
-				+ "  \"password\": \"Munna@4457\"\r\n"
-				+ "}");
-		Response response = y.post("/api/auth/login");
+		
+		LoginRequest loginRequest = new LoginRequest("Raubin","Munna@4457");
+		AuthService authService = new AuthService();
+		Response response = authService.login(loginRequest);
+		
+		LoginResponse loginResponse = response.as(LoginResponse.class);
+		
 		System.out.println(response.asPrettyString());
-		Assert.assertEquals(response.getStatusCode(), 200);
+		System.out.println("Token : "+loginResponse.getToken());
+		System.out.println("Id : "+loginResponse.getId());
+		System.out.println("Email : "+loginResponse.getEmail());
+		
+		Assert.assertTrue(loginResponse.getToken() != null);
+		Assert.assertEquals(loginResponse.getId(), 3720);
+		Assert.assertEquals(loginResponse.getEmail(), "raubinkumar4321@gmail.com");
+		
 	}
 
 }
